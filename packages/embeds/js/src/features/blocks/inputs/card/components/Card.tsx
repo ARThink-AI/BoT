e928b8533cc401a3a0e50f839566befc1fc277f2
hakimuddin-arthink <c-hakimuddin.s@arthink.ai>
2024-04-04 11:11:09 +0530

    card updated

--- packages/embeds/js/src/features/blocks/inputs/card/components/Card.tsx
+++ packages/embeds/js/src/features/blocks/inputs/card/components/Card.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For, createEffect } from 'solid-js';
+import { createSignal, For } from 'solid-js';
 import './style.css'
 
 export const CardInput = (props: any) => {
@@ -17,11 +17,11 @@ export const CardInput = (props: any) => {
   // }
 
 
-  const updateInput = (property: string, value: any, index: Number) => {
+  const updateInput = (property: string, value: any, index: number) => {
     // @ts-ignore
-    let inputss = inputs().map((inp, ind) => {
+    const inputss = inputs().map((inp, ind) => {
       if (ind != index) return inp
-      let modified = { ...inputs()[ind] };
+      const modified = { ...inputs()[ind] };
 
       modified[property] = value;
       return modified
@@ -29,9 +29,19 @@ export const CardInput = (props: any) => {
     setInputs(inputss);
   }
 
-  createEffect(() => {
+  const handleSubmit = () => {
+    const ans = {};
+    inputs().forEach(input => {
+      ans[input.answerVariableId] = input.userInput;
+    });
+    console.log("Submitted:", ans);
+    if (props.onSubmit) {
+      props.onSubmit({ label: "Submitted", value: JSON.stringify(ans) });
+    } else {
+      console.error("onSubmit prop is not provided.");
+    }
+  };
 
-  }, updateInput)
 
   console.log("card props", props)
 
@@ -39,7 +49,7 @@ export const CardInput = (props: any) => {
 
     <>
       <div class="mx-auto">
-        <div class="p-3 lg-w-[450px] h-[400px] sm-w-full rounded-md shadow-lg shadow-black-50 overflow-hidden">
+        <div class="p-6 lg-w-[450px] h-[400px] sm-w-full rounded-md shadow-lg shadow-black-50 overflow-hidden">
           <div class="flex flex-col h-full gap-2">
             <div id="headings">
               <p class="sticky top-0 bg-white text-xl">{props.block.options.heading}</p>
@@ -48,56 +58,153 @@ export const CardInput = (props: any) => {
               </p>
             </div>
             <div class="overflow-y-scroll hide-scrollbar flex-1 mb-2" id="input-container">
+
               {inputs().map((input: any, i: number) => {
                 switch (input.type) {
                   case "text":
                     return (
-                      <input
+                      <>
+                        <label for="">{input.label}</label>
+                        <input
 
-                        type={input.type}
-                        placeholder={input.placeholder}
-                        class={`border p-2 rounded-md w-full mb-2`}
-                        value={input?.userInput ? input?.userInput : ""}
-                        onInput={(e) =>
-                          updateInput("userInput", e.target.value, i)
-                        }
-                        autofocus
+                          type={input.type}
+                          placeholder={input.placeholder}
+                          class={`border p-2 rounded-md w-full mb-2`}
+                          value={input?.userInput ? input?.userInput : ""}
+                          onChange={(e) =>
+                            updateInput("userInput", e.target.value, i)
+                          }
 
 
-                        required={input.required}
-                      />
 
+                          required={input.required}
+                        />
+                      </>
                     );
                   case "email":
                     return (
-                      <input
-                        type={input.type}
-                        placeholder={input.placeholder}
-                        class={`border p-2 rounded-md w-full mb-2`}
-                        value={input?.userInput ? input?.userInput : ""}
-                        onInput={(e) =>
-                          updateInput("userInput", e.target.value, i)
-                        }
+                      <>
+                        <label for="">{input.label}</label>
+                        <input
+                          type={input.type}
+                          placeholder={input.placeholder}
+                          class={`border p-2 rounded-md w-full mb-2`}
+                          value={input?.userInput ? input?.userInput : ""}
+                          onChange={(e) =>
+                            updateInput("userInput", e.target.value, i)
+                          }
 
-                        required={input.required}
-                      />
-
+                          required={input.required}
+                        />
+                      </>
                     );
                   case "phone":
                     return (
-                      <input
-                        type={input.type}
-                        placeholder={input.placeholder}
-                        class={`border p-2 rounded-md w-full mb-2`}
-                        value={input?.userInput ? input?.userInput : ""}
-                        onInput={(e) =>
-                          updateInput("userInput", e.target.value, i)
-                        }
+                      <>
+                        <label for="">{input.label}</label>
+                        <input
+                          type={input.type}
+                          placeholder={input.placeholder}
+                          class={`border p-2 rounded-md w-full mb-2`}
+                          value={input?.userInput ? input?.userInput : ""}
+                          onChange={(e) =>
+                            updateInput("userInput", e.target.value, i)
+                          }
 
-                        required={input.required}
-                      />
-
+                          required={input.required}
+                        />
+                      </>
                     );
+
+                  case "dropdown":
+                    return (
+                      <>
+                        <label for="">{input.label}</label>
+                        <select
+                          class="w-full p-2 appearance-none border rounded-md mb-2"
+                          value={input?.userInput ? input?.userInput : ""}
+                          onChange={(e) => updateInput("userInput", e.target.value, i)}
+                          required={input.required}
+                        >
+                          <option value="" disabled selected>
+                            {input.placeholder}
+                          </option>
+                          <For each={input.values}>{(value) => (
+                            <option value={value}>{value}</option>
+                          )}</For>
+                        </select >
+                      </>
+                    )
+
+                  case "textarea":
+                    return (
+                      <>
+                        <label>{input.label}</label>
+                        < textarea
+                          class="border p-2 rounded-md w-full"
+                          placeholder={input.placeholder}
+                          value={input?.userInput ? input?.userInput : ""}
+                          onChange={(e) =>
+                            updateInput("userInput", e.target.value, i)}
+                          required={input.required}
+                        />
+                      </>
+                    )
+                  case "radio":
+                    return (
+                      <><label for="">{input.label}</label>
+                        <div class="flex justify-start gap-1 mt-2">
+
+                          <For each={input.values}>{(value) => (
+                            <>
+                              <input
+                                id={value}
+                                type="radio"
+                                class="border p-2 h-6 w-6 rounded-md mb-2 accent-[#0077CC]"
+                                value={value}
+                                checked={input.userInput === value}
+                                onChange={(e) =>
+                                  updateInput("userInput", e.target.value, i)}
+                                required={input.required}
+                              />
+                              <label for={value}>{value}</label>
+                            </>
+                          )}</For>
+                        </div>
+                      </>)
+
+                  case "checkbox":
+                    return (
+                      <>
+                        <label for="">{input.label}</label>
+                        <div class="flex justify-start flex-col gap-1">
+                          <For each={input.values}>{(value) => (
+                            <div class="flex justify-start gap-2">
+                              <input
+                                type="checkbox"
+                                class="border p-2 h-6 w-6 rounded-md mb-2 accent-[#0077CC]"
+                                value={value}
+                                onChange={(e) => {
+                                  const isChecked = e.target.checked;
+                                  let updatedValues;
+                                  if (!input.userInput) {
+                                    updatedValues = isChecked ? [value] : [];
+                                  } else {
+                                    updatedValues = isChecked
+                                      ? [...input.userInput, value]
+                                      : input.userInput.filter((item) => item !== value);
+                                  }
+                                  updateInput("userInput", updatedValues, i);
+                                }}
+                                checked={input.userInput && input.userInput.includes(value)}
+                              />
+                              <label>{value}</label>
+                            </div>
+                          )}</For>
+
+                        </div >
+                      </>
+                    )
 
 
 
@@ -114,13 +221,13 @@ export const CardInput = (props: any) => {
                   ans[props?.block?.options?.inputs[i].answerVariableId] =
                 }
               }}  */}
-              <button class="rounded-full w-[95px] h-[40px] bg-[#0077CC] text-white mt-2">
+              <button onClick={handleSubmit} class="rounded-full w-[95px] h-[40px] bg-[#0077CC] text-white mt-2">
                 Submit
               </button>
             </div>
           </div>
-        </div>
-      </div>
+        </div >
+      </div >
 
 
     </>
