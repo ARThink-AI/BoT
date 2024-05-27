60bcfb6275de225cdf9344dc81db297461f5d9ce
hakimuddin-arthink <c-hakimuddin.s@arthink.ai>
2024-05-27 14:48:16 +0530

    small changes

--- apps/builder/src/features/analytics/api/getTotalAnswersInBlocks.ts
+++ apps/builder/src/features/analytics/api/getTotalAnswersInBlocks.ts
@@ -101,7 +101,101 @@ export const getTotalAnswersInBlocks = authenticatedProcedure
     // })
     // console.log('count aggregations', totalAnswersPerContent)
 
-    console.log('answer content', totalAnswersPerContent)
+    // console.log('answer content', totalAnswersPerContent)
+
+    // function getTotalCounts(data) {
+    //   const contentTotals = {}
+
+    //   data.forEach((item) => {
+    //     const contents = item.content
+    //       .split(',')
+    //       .map((content) => content.trim())
+
+    //     contents.forEach((content) => {
+    //       if (!contentTotals[content]) {
+    //         contentTotals[content] = 0
+    //       }
+    //       contentTotals[content] += item._count._all
+    //     })
+    //   })
+
+    //   return contentTotals
+    // }
+
+    // // Get the total counts for each unique content type
+    // const totalCounts = getTotalCounts(totalAnswersPerContent)
+
+    // console.log(totalCounts)
+
+    const inputType = [
+      'choice input',
+      'number input',
+      'email input',
+      'date input',
+      'phone number input',
+      'text input',
+      'url input',
+    ]
+
+    // const totalAnswersPerInputTypes = await prisma.answer.groupBy({
+    //   by: ['blockId', 'content'], // Group by blockId and content
+    //   where: {
+    //     result: {
+    //       typebotId: typebot.publishedTypebot.typebotId,
+    //     },
+    //     blockId: {
+    //       in: publishedTypebot.groups.flatMap((group) =>
+    //         group.blocks
+    //           .filter((block) => inputType.includes(block.type))
+    //           .map((block) => block.id)
+    //       ),
+    //     },
+    //   },
+    //   _count: {
+    //     _all: true,
+    //     // Count the number of answers
+    //   },
+    // })
+
+    // function getTotalCounts(totalAnswersPerInputTypes) {
+    //   const contentTotals = {}
+
+    //   totalAnswersPerInputTypes.forEach((item) => {
+    //     const contents = item.content
+    //       .split(',')
+    //       .map((content) => content.trim())
+
+    //     contents.forEach((content) => {
+    //       if (!contentTotals[content]) {
+    //         contentTotals[content] = 0
+    //       }
+    //       contentTotals[content] += item._count._all
+    //     })
+    //   })
+
+    //   return contentTotals
+    // }
+
+    // // Get the total counts for each unique content type
+    // const totalCounts = getTotalCounts(totalAnswersPerInputTypes)
+
+    // console.log('base on type', totalCounts)
+
+    const totalAnswersInputPerBlock = await prisma.answer.groupBy({
+      by: ['itemId', 'blockId'],
+      where: {
+        result: {
+          typebotId: typebot.publishedTypebot.typebotId,
+        },
+        blockId: {
+          in: publishedTypebot.groups.flatMap((group) =>
+            group.blocks.map((block) => block.id)
+          ),
+        },
+      },
+
+      _count: { _all: true },
+    })
 
     return {
       totalAnswersInBlocks: totalAnswersPerBlock.map((answer) => ({
